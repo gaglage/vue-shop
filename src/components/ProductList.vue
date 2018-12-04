7b2a4de01de01e6d0f41630e292c2ff66377e3ec
Gabriel Alvarez <gabrielalvarez@dorna.com>
2018-12-04 16:52:44 +0100

    pagination

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>{{products.length}}</div>
+  <div v-if="products.length">
+    <paginate
+        name="products"
+        :list="products"
+        :per="perPage"
+    >
+    <p v-for="product in paginated('products')" :key="product.id"> {{ product.name }}</p>
+    </paginate>
+    <paginate-links
+        for="products"
+        :classes="{
+            'ul': 'pagination',
+            'li': 'page-item',
+            'li > a': 'page-link'
+        }"
+    ></paginate-links>
+
+  </div>
+  <b-alert v-else show variant="info">No hay productos que mostrar</b-alert>
 </template>
 
 <script>
@@ -8,11 +26,11 @@ export default {
   mounted() {
     this.fetchProducts();
   },
-  data(){
-      return {
-          paginate: ['products'],
-          perPage: 3
-      }
+  data() {
+    return {
+      paginate: ["products"],
+      perPage: 3
+    };
   },
   computed: {
     ...mapState("products", ["products"])
