343cddf20d7c42b65ac219f05fd5394575e93932
gabrielalvarez <gabrielalvarez@dorna.com>
2018-12-11 16:40:13 +0100

    persist indexeddb

--- src/store.js
+++ src/store.js
@@ -1,17 +1,29 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import localForage from "localforage";
 
 import VuexPersistence from 'vuex-persist'
 import products from './modules/products'
 import cart from './modules/cart'
 
 Vue.use(Vuex)
+
+// LocalStorage
 // https://github.com/championswimmer/vuex-persist#readme
+// const vuexLocal = new VuexPersistence({
+//   storage: window.localStorage,
+//   modules: ['cart']
+// })
+
+
+// IdexedDB
+//https://github.com/localForage/localForage
+
 const vuexLocal = new VuexPersistence({
-  storage: window.localStorage,
+  storage: localForage,
   modules: ['cart']
 })
+
 
 export default new Vuex.Store({
   modules: {
